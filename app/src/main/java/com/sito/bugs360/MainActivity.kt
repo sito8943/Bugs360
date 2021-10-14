869d9b798312802bd9cc67e499796df1cd4d427e
SitoNimbus <sito8943@gmail.com>
2021-10-14 15:24:03 -0700

    on zoom can be move

--- app/src/main/java/com/sito/bugs360/MainActivity.kt
+++ app/src/main/java/com/sito/bugs360/MainActivity.kt
@@ -14,10 +14,14 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.sito.bugs360.ui.theme.Bugs360Theme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
 
@@ -51,17 +55,31 @@ fun ShowImage() {
         mutableStateOf(R.drawable.ixodus_ricinus_a)
     }
 
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+
     // set up all transformation states
     var scale by remember { mutableStateOf(1f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
     val state = rememberTransformableState { zoomChange, offsetChange, _ ->
         scale *= zoomChange
+        if (scale < 1f)
+            scale = 1f
         offset += offsetChange
     }
 
     Column() {
         Image(
             modifier = Modifier
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onDoubleTap = {
+                            if (scale == 1f) {
+                                scale = 2f
+                            } else scale = 1f
+                        },
+                    )
+                }
                 // apply other transformations like rotation and zoom
                 // on the pizza slice emoji
                 .graphicsLayer(
@@ -76,23 +94,26 @@ fun ShowImage() {
                 .fillMaxSize()
                 //drag
                 .padding(5.dp)
+                .offset { IntOffset(offsetX.roundToInt(), 0) }
                 .draggable(
                     orientation = Orientation.Horizontal,
                     state = rememberDraggableState { delta ->
-                        d = image
-                        del = delta
-                        if (del > 0) {
-                            d++
-                        } else {
-                            d--
-                        }
-                        if (d > start + 17)
-                            d = start
-                        else if (d < start)
-                            d = start + 17
-                        image = d
-                        del = delta
-                        //offsetX = delta
+                        if (scale == 1f) {
+                            d = image
+                            del = delta
+                            if (del > 0) {
+                                d++
+                            } else {
+                                d--
+                            }
+                            if (d > start + 17)
+                                d = start
+                            else if (d < start)
+                                d = start + 17
+                            image = d
+                            del = delta
+                        } else
+                            offsetX += delta
                         delta
                     }
                 ),
